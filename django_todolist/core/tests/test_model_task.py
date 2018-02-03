a9981c0482452a2913d0100ae27827fb65fccc22
MarioGN <mario.geroldineto@gmail.com>
2018-02-03 17:11:16 -0200

    TaskModelTest.text_field

--- django_todolist/core/tests/test_model_task.py
+++ django_todolist/core/tests/test_model_task.py
@@ -13,3 +13,14 @@ class TaskModelTest(TestCase):
 
     def test_created(self):
         self.assertTrue(Task.objects.exists())
+
+    def test_text_value(self):
+        self.assertEqual('My Task', self.task.text)
+
+    def test_text_blank(self):
+        field = Task._meta.get_field('text')
+        self.assertFalse(field.blank)
+
+    def test_text_null(self):
+        field = Task._meta.get_field('text')
+        self.assertFalse(field.null)
