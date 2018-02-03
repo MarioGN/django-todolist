b63ff82e6d13acc23441afec85598cfaa3ad9856
MarioGN <mario.geroldineto@gmail.com>
2018-02-03 17:16:32 -0200

    Refactor TaskModelTest.text_field

--- django_todolist/core/tests/test_model_task.py
+++ django_todolist/core/tests/test_model_task.py
@@ -7,6 +7,7 @@ class TaskModelTest(TestCase):
 
     def setUp(self):
         self.task = Task.objects.create(text='My Task')
+        self.text_field = Task._meta.get_field('text')
 
     def tearDown(self):
         self.task.delete()
@@ -17,10 +18,8 @@ class TaskModelTest(TestCase):
     def test_text_value(self):
         self.assertEqual('My Task', self.task.text)
 
-    def test_text_blank(self):
-        field = Task._meta.get_field('text')
-        self.assertFalse(field.blank)
+    def test_text_cant_be_blank(self):
+        self.assertFalse(self.text_field.blank)
 
-    def test_text_null(self):
-        field = Task._meta.get_field('text')
-        self.assertFalse(field.null)
+    def test_text_cant_be_null(self):
+        self.assertFalse(self.text_field.null)
