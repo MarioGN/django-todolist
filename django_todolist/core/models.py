95fe52dd46a0411bc012a52b0f1052452004d761
MarioGN <mario.geroldineto@gmail.com>
2018-02-04 21:21:27 -0200

    Add active field to core.models.Task

--- django_todolist/core/models.py
+++ django_todolist/core/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class Task(models.Model):
-    text = models.CharField('Tarefa', max_length=200)
+    text = models.CharField('Task', max_length=200)
     created_at = models.DateField(auto_now_add=True)
     completed = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
 
     def complete(self):
         self.completed = True
