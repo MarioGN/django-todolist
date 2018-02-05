ed3d477c025ec9954e3574464754d409c9c57de1
MarioGN <mario.geroldineto@gmail.com>
2018-02-05 17:23:07 -0200

    Apply filter active=True on task list

--- django_todolist/core/views.py
+++ django_todolist/core/views.py
@@ -4,7 +4,7 @@ from .models import Task
 
 
 def index(request):
-    task_list = Task.objects.all()
+    task_list = Task.objects.filter(active=True)
     template_name = 'core/index.html'
 
     return render(request, template_name, {'task_list': task_list})
