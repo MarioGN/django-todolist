a92de90d91b98adb5286e4ac21508948010daec2
MarioGN <mario.geroldineto@gmail.com>
2018-02-05 12:00:51 -0200

    IndexViewTest.test_has_context_list

--- django_todolist/core/tests/test_view_index.py
+++ django_todolist/core/tests/test_view_index.py
@@ -1,5 +1,8 @@
+from django.db.models.query import QuerySet
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
+
+from django_todolist.core.models import Task
 
 
 class IndexViewGet(TestCase):
@@ -14,3 +17,7 @@ class IndexViewGet(TestCase):
     def test_template(self):
         """Must user 'core/index.html'"""
         self.assertTemplateUsed(self.resp, 'core/index.html')
+
+    def test_has_context_list(self):
+        context_list = self.resp.context['task_list']
+        self.assertIsInstance(context_list, QuerySet)
