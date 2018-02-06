2ea9e0209988a9886f9e7fed7a4bd7c89f0a4aaf
MarioGN <mario.geroldineto@gmail.com>
2018-02-06 14:07:45 -0200

    Redirect invalid filter to 404

--- django_todolist/core/views.py
+++ django_todolist/core/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from .models import Task
@@ -13,5 +14,7 @@ def index(request, filter='current'):
         task_list = Task.objects.filter(active=True).filter(completed=True)
     elif filter == 'all':
         task_list = Task.objects.all()
+    else:
+        raise Http404
 
     return render(request, template_name, {'task_list': task_list})
