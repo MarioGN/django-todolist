c8491b2b4f690b046e55f851e4cf5387db93229b
MarioGN <mario.geroldineto@gmail.com>
2018-02-06 00:05:09 -0200

    Refactor IndexView.test_filter_active

--- django_todolist/core/tests/test_view_index.py
+++ django_todolist/core/tests/test_view_index.py
@@ -2,13 +2,21 @@ from django.db.models.query import QuerySet
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
 
+from model_mommy import mommy
+
 from django_todolist.core.models import Task
 
 
 class IndexViewGet(TestCase):
 
     def setUp(self):
+        # deleted tasks
+        mommy.make('core.Task', active=False, _quantity=4)
+        mommy.make('core.Task', _quantity=6)
         self.resp = self.client.get(r('core:index'))
+
+    def tearDown(self):
+        Task.objects.all().delete()
 
     def test_get(self):
         """Get must return status code 200"""
@@ -23,6 +31,5 @@ class IndexViewGet(TestCase):
         self.assertIsInstance(context_list, QuerySet)
 
     def test_list_filter_active(self):
-        task_list = Task.objects.filter(active=True)
         context_list = self.resp.context['task_list']
-        self.assertEqual(list(task_list), list(context_list))
+        self.assertEqual(6, context_list.count())
