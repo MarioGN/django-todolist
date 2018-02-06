aea43c47f7fc481344940977c99cc62db074ceb6
MarioGN <mario.geroldineto@gmail.com>
2018-02-06 15:57:50 -0200

    Add class active to button group

--- django_todolist/core/views.py
+++ django_todolist/core/views.py
@@ -7,14 +7,18 @@ from .models import Task
 def index(request, filter='current'):
     task_list = []
     template_name = 'core/index.html'
+    active_button = 'current'
+
 
     if filter == 'current':
         task_list = Task.objects.filter(active=True).filter(completed=False)
     elif filter == 'completed':
+        active_button = 'completed'
         task_list = Task.objects.filter(active=True).filter(completed=True)
     elif filter == 'all':
+        active_button = 'all'
         task_list = Task.objects.all()
     else:
         raise Http404
 
-    return render(request, template_name, {'task_list': task_list})
+    return render(request, template_name, context)
