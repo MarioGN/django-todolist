bb40db95fd27183c0d8abd45674c89843d72c452
MarioGN <mario.geroldineto@gmail.com>
2018-02-06 14:30:54 -0200

    IndexViewsFilterTest.test_index_list

--- django_todolist/core/tests/test_view_index.py
+++ django_todolist/core/tests/test_view_index.py
@@ -33,3 +33,22 @@ class IndexViewGet(TestCase):
     def test_list_filter_active_completed(self):
         context_list = self.resp.context['task_list']
         self.assertEqual(6, context_list.count())
+
+
+class IndexViewFilters(TestCase):
+
+    def setUp(self):
+        self.open_tasks = mommy.make('core.Task', _quantity=3)
+        self.completed_tasks = mommy.make(
+            'core.Task', completed=True, _quantity=5
+        )
+        # deleted
+        mommy.make('core.Task', active=False, _quantity=2)
+
+    def tearDown(self):
+        Task.objects.all().delete()
+
+    def test_index_list(self):
+        resp = self.client.get(r('core:index'))
+        context_list = resp.context['task_list']
+        self.assertEqual(len(context_list), len(self.open_tasks))
