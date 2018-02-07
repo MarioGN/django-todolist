3b9fd9cf6372b074a8876162983711413e75073e
MarioGN <mario.geroldineto@gmail.com>
2018-02-07 00:02:07 -0200

    Extract more logic and clean index view

--- django_todolist/core/views.py
+++ django_todolist/core/views.py
@@ -5,24 +5,19 @@ from .models import Task
 
 
 def index(request, filter='current'):
+
+    if not filter_is_valid(filter):
+        raise Http404
+
     task_list = []
     template_name = 'core/index.html'
-    active_button = 'current'
 
-    current_count = Task.objects.filter(active=True).filter(completed=False).count()
-    completed_count = Task.objects.filter(active=True).filter(completed=True).count()
-    all_count = Task.objects.all().count()
+    current_count = get_context_list(filter='current').count()
+    completed_count = get_context_list(filter='completed').count()
+    all_count = get_context_list(filter='all').count()
 
-    if filter == 'current':
-        task_list = get_context_list(filter)
-    elif filter == 'completed':
-        active_button = 'completed'
-        task_list = get_context_list(filter)
-    elif filter == 'all':
-        active_button = 'all'
-        task_list = get_context_list(filter)
-    else:
-        raise Http404
+    task_list = get_context_list(filter)
+    active_button = filter
 
     context = {
         'task_list': task_list,
@@ -47,3 +42,9 @@ def get_context_list(filter):
         task_list = Task.objects.all()
 
     return task_list
+
+
+def filter_is_valid(filter):
+    if filter != 'current' and filter != 'completed' and filter != 'all':
+        return False
+    return True
