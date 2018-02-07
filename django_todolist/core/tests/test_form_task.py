8dd6b7dddd79674247ec2fb86d85c2997ace1809
MarioGN <mario.geroldineto@gmail.com>
2018-02-07 13:32:03 -0200

    TaskFormTest.test_text_is_required

--- django_todolist/core/tests/test_form_task.py
+++ django_todolist/core/tests/test_form_task.py
@@ -10,3 +10,20 @@ class TaskFormTest(TestCase):
         form = TaskForm()
         expected = ['text', ]
         self.assertSequenceEqual(expected, list(form.fields))
+
+    def test_text_is_required(self):
+        """Text field is required"""
+        form = self.make_validated_form(text='')
+        self.assertTrue(form.errors)
+        
+        errors = form.errors.as_data()
+        errors_list = errors['text']
+        exception = errors_list[0]
+        self.assertEqual('required', exception.code)
+
+    def make_validated_form(self, **kwargs):
+        valid = dict(text='My task')
+        data = dict(valid, **kwargs)
+        form = TaskForm(data)
+        form.is_valid()
+        return form
