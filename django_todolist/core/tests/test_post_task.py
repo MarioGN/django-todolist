de335ef0ce08911421ff41f5fabc118a3c59fbcf
MarioGN <mario.geroldineto@gmail.com>
2018-02-07 15:16:51 -0200

    PostTaskForm.test_post_index

---
+++ django_todolist/core/tests/test_post_task.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.shortcuts import resolve_url as r
+
+from django_todolist.core.models import Task
+from django_todolist.core.forms import TaskForm
+
+
+class PostTaskForm(TestCase):
+
+    def setUp(self):
+        self.count = Task.objects.all().count()
+        self.data = dict(text='My new task')
+
+    def tearDown(self):
+        Task.objects.all().delete()
+
+    def test_post_create_index(self):
+        self.client.post(r('core:index'), self.data)
+        self.assertEqual(self.count+1, Task.objects.all().count())
