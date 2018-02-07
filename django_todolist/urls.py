8ea5eed890b56f4feb337cf97c3599b9497b47f2
MarioGN <mario.geroldineto@gmail.com>
2018-02-07 00:39:05 -0200

    Fix url match

--- django_todolist/urls.py
+++ django_todolist/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include('django_todolist.core.urls')),
     path('admin/', admin.site.urls),
+    path('', include('django_todolist.core.urls')),
 ]
