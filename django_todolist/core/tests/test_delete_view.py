0442eef8679c3f7e48aea0deb4656f0d8d9baa97
MarioGN <mario.geroldineto@gmail.com>
2018-02-08 00:02:20 -0200

    DeleteViewGet.test_delete_view

---
+++ django_todolist/core/tests/test_delete_view.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.shortcuts import resolve_url as r
+
+from django_todolist.core.models import Task
+
+
+class DeleteViewGet(TestCase):
+
+    def setUp(self):
+        self.task = Task.objects.create(text='My new task')
+
+    def tearDown(self):
+        Task.objects.all().delete()
+
+    def test_delete_view(self):
+        count = Task.objects.all().count()
+        self.client.get(r('core:delete', pk=self.task.pk))
+        self.assertTrue(count-1, Task.objects.all().count())
