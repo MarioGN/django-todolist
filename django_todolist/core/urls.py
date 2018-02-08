50b86c9ed70d056ba51842efae5153cb5a4bbe56
MarioGN <mario.geroldineto@gmail.com>
2018-02-08 17:37:48 -0200

    Create complete url

--- django_todolist/core/urls.py
+++ django_todolist/core/urls.py
@@ -7,5 +7,6 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('delete/<int:pk>', views.delete_task, name='delete'),
+    path('complete/<int:pk>', views.complete_task, name='complete'),
     path('<str:filter>/', views.index, name='index_filter'),
 ]
