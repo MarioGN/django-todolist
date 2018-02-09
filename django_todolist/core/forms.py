7903c820db414f36b6c6a8a97f6483c13dc4e6bc
MarioGN <mario.geroldineto@gmail.com>
2018-02-09 11:02:17 -0200

    Create core.forms.TaskEditForm

--- django_todolist/core/forms.py
+++ django_todolist/core/forms.py
@@ -14,3 +14,13 @@ class TaskForm(forms.ModelForm):
                 attrs={'class': 'form-control', 'placeholder': 'New task'}
             ),
         }
+
+
+class TaskEditForm(forms.Form):
+
+    id_field = forms.CharField(widget=forms.HiddenInput())
+    text = forms.CharField(
+        label='Description',
+        widget=forms.Textarea(attrs={'rows': 3, 'class': 'form-control'}),
+        required=True,
+    )
